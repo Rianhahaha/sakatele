f3aa9bbf1613c38148e2339ddcf44430631c6bca
Rianhahaha <triandiapriliio@gmail.com>
2024-02-11 20:33:14 +0700

    first commit

---
+++ dist/js/custom.js
@@ -0,0 +1,56 @@
+// slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " actived";
+}
+
+
+var className = "scrolled";
+var scrollTrigger = 60;
+
+window.onscroll = function() {
+  // We add pageYOffset for compatibility with IE.
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByTagName("header")[0].classList.add(className);
+  } else {
+    document.getElementsByTagName("header")[0].classList.remove(className);
+  }
+};
+
+      function openNav() {
+        document.getElementById("resp").style.top = "0px";
+    }
+      function closeNav() {
+        document.getElementById("resp").style.top = "-500px";
+    }
+
+// var X = document.getElementsByClassName('nav-resp');
+// function navbarBurger() {
+//   if(X.style.position === 'fixed') {
+//     X.style.top = '-100px';
+//   } else {
+//     X.style.top = '1px'
+//   }
+// }
